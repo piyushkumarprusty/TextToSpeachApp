3e3a545fac55880f03a32ecc4dd85c6e3f5d64a6
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-02 19:40:36 +0530

    init

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText e1;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e1 = findViewById(R.id.e1);
+        b1= findViewById(R.id.b1);
+        
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakOn();
+            }
+        });
+    }
+
+    private void speakOn() {
     }
 }
