14f0fbeb25d63afcfd2243bf358f78b78761f2dc
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-02 19:48:23 +0530

    validation for language

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -31,7 +31,12 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         });
     }
 public void onInit(int status){
-
+if (status == TextToSpeech.SUCCESS){
+    int result =tts.setLanguage(Locale.US);
+    if(result==TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+        Toast.makeText(getApplicationContext(),"Language Not Supported" , Toast.LENGTH_SHORT).show();
+    }
+}
 }
 
 
